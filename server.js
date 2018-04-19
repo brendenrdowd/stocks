6052065f81302f80d031cdd56ece9df84625c144
Brendendowd <brenden.dowd@yahoo.com>
2018-04-18 17:04:28 -0700

    changed search parameters and cleaned up UI

--- server.js
+++ server.js
@@ -17,34 +17,26 @@ app.set('view engine', 'ejs');
 
 //routing
 app.get('/', function(req,res){
-    console.log("on render:", stockObj)
     res.render("index",{'stockObj':stockObj});
 })
 app.post('/search',function(req,res){
-    console.log('req.body', req.body.ticker)
     let data = req.body.ticker;
     data = data.replace(/\+/g, ' ').replace(/,/gi,"").split(" ");
-    console.log("clean data", data)
     apiStr = "";
     for(let ticker of data){
         ticker = ticker.trim();
-        console.log("looping", ticker);
         if(ticker.length < 10 && ticker.length > 0){
-            console.log("if-ing")
             if(apiStr.length < 1){
                 apiStr = ticker;
             }else{
                 apiStr = apiStr + ',' + ticker;
             }
-            console.log("apiString",apiStr);
         }else{
             continue;
         }
     }
-    console.log("hitting api");
     request(`https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=${apiStr}&apikey=8X3HNXXJSGXLQ8XN`, function(error,response,body){
         if(!error && response.statusCode == 200){
-            console.log("post api",JSON.parse(body));
             stockObj = JSON.parse(body);
             res.redirect('/');
         }else{
